c1b247de7be9b92040e81820e79fa1cc01743a5f
Sergey <mantikor@tut.by>
2016-10-25 15:50:57 +0300

    Add files via upload

---
+++ snowden.py
@@ -0,0 +1,24 @@
+import sys
+
+if len(sys.argv) == 2: #check input file
+    inFile = sys.argv[1]
+else:
+    print('no valid file name...')
+    sys.exit()
+
+testStr = open(inFile).read() #read data to single string
+testList = [testStr[i] for i in range(len(testStr))] #convert string to list for best perfomance and low memory use
+k = 0
+while k < (len(testList) - 1):
+    if testList[k] == testList[k + 1]: #checking pair of symbols
+        testList.pop(k) #del first element of double
+        testList.pop(k) #del secont elemrnt (it comes first place)
+        if k != 0: #if remove two symbols, shift back to check previous
+            k -= 1
+        continue
+    else:
+        k += 1 #if symbols not equal go to next
+
+resStr = ''.join(testList) #list to string
+
+print(resStr)
